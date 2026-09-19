b6a9ddee2cc5ab20fffc6884771a41df423f8005
Clarence S. Japinan <94746830+JapinanClarence@users.noreply.github.com>
2026-09-19 22:01:39 +0800

    🔥 delete: unused images assets

--- apps/web/constants/images.ts
+++ apps/web/constants/images.ts
@@ -1,15 +1,8 @@
 import bauyot from "@/assets/images/Bauyot, Mary Fil M.png";
-import tahoy from "@/assets/images/Tahoy, Genesesly.png";
 import orencita from "@/assets/images/Lozada, Orencita Aireen V.png";
 import sadie from "@/assets/images/Law-ay, Sadie D.png";
-import cecile from "@/assets/images/Lofranco, Cecile Treasurer.png";
-import vaneza from "@/assets/images/Paquiao, Vaneza.png";
-import sarah from "@/assets/images/Aranges, Sarah C.png";
-import corazon from "@/assets/images/Umblero, Corazon.png";
 import jeralyn from "@/assets/images/Hemillan, Jeralyn N.png";
 import helina from "@/assets/images/Dupa, Helina Jean P.png";
-import joyce from "@/assets/images/Jasa, Joyce.png";
-import imelda from "@/assets/images/Lauron, Imelda.png";
 import villegas from "@/assets/images/Villegas, Jhonnel P.png";
 import evelyn from "@/assets/images/Ecle, Evelyn S.png";
 import canoy from "@/assets/images/Canoy, Fe T.png";
@@ -38,17 +31,10 @@ import open_access_logo from "@gad/assets/images/Open Access Logo.png";
 
 export const images = {
   bauyot,
-  tahoy,
   orencita,
   sadie,
-  cecile,
-  vaneza,
-  sarah,
-  corazon,
   jeralyn,
   helina,
-  joyce,
-  imelda,
   villegas,
   evelyn,
   canoy,
